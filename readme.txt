1f034eaba34e606e3a9b5963957fa6500fcab126
oya3 <terje@oya3.net>
2013-07-31 22:04:03 +0900

    初回コミット

---
+++ readme.txt
@@ -0,0 +1,16 @@
+機能：
+SVN で管理されたソース差分を生成する
+
+使い方：
+perl svndiff.pl [options] <svn address> <output path>
+
+仕様：
+指定された svn address の開始、終了（最新）リビジョンの差分を取得する。
+オプションで開始、終了リビジョンを指定することも可能。
+
+動作確認：
+windows 7(32,64)環境のみ
+
+同梱内容：
+svndiff.pl
+svndiff.exe
---
+++ svndiff.pl
@@ -0,0 +1,261 @@
+#<!-- -*- encoding: utf-8n -*- -->
+use strict;
+use warnings;
+use utf8;
+
+use Cwd;
+use Encode;
+use Encode::JP;
+
+#use File::Path;
+#use File::Copy 'copy';
+# mkpath は日本語文字列はうまく動作しない（バグがある。/test表示 の場合、testフォルダとtest表示フォルダの２個作成される)
+# windows command が文字化けしてしまうので使わない。
+# use open IN   => ":encoding(cp932)"; # 入力ファイルはcp932
+# use open OUT  => ":encoding(cp932)"; # 出力ファイルはcp932
+# binmode STDIN, ':encoding(cp932)'; # 標準入力はcp932
+# binmode STDOUT, ':encoding(cp932)'; # 標準出力はcp932
+# binmode STDERR, ':encoding(cp932)'; #エラー出力はcp932
+# binmode STDOUT => ":utf8";
+
+#_mkdir('/output/test/test2\test3\test4');
+#exit;
+
+my $mypath = __FILE__;            # ファイル名がパスつきで入ってきます。
+$mypath =~ s/^(.+)[\\\/].+$/$1/;  # 最後の\か/までマッチしたもので置き換え
+$mypath =~ s/\\/\//g;
+
+print "svndiff ver. 0.13.07.29.\n";
+my ($argv, $gOptions) = getOptions(\@ARGV); # オプションを抜き出す
+my $args = @{$argv};
+
+if( $args != 2 ){
+	print "Usage: svndiff [options] <svn address> <output path>\n";
+	print "  options : -u : user.\n";
+	print "          : -p : password.\n";
+	print "          : -r (start:end) : revsion number.\n";
+	print "          : -report (fileNmae) : output add/del/mod report.\n";
+    exit;
+}
+
+if( !isInstallSVN() ){
+	die "svn not installed.";
+}
+
+my $fileList = undef;
+my $srev = '';
+my $erev = '';
+my $address = $argv->[0];
+my $outputPath  = $argv->[1];
+
+# オプション指定でリビジョンが存在か確認
+if( exists $gOptions->{'-r_start'} ){
+	# オプション指定がある
+	$srev = $gOptions->{'-r_start'};
+	$erev = $gOptions->{'-r_end'};
+}
+else{
+	# ブランチの最初と最後のリビジョンを取得する
+	($srev, $erev, $fileList) = getRevisionNumber($address);
+}
+my $diffres = svnCmd("diff","-r $srev:$erev", $address, "");
+my $files = getDiffFileList($diffres);
+
+exportFiles($srev, $address, $files, $outputPath);
+exportFiles($erev, $address, $files, $outputPath);
+if( exists $gOptions->{'-report'} ){
+	exportReport( "$outputPath\/$gOptions->{'-report'}", $fileList);
+}
+print "complate.\n";
+exit;
+
+
+sub exportReport
+{
+	my ($file,$fileList) = @_;
+
+	my $file_sjis = encode('cp932', $file);
+	my %keys = ("A"=>"追加", "D"=>"削除", "M"=>"変更", "R"=>"置換");
+	
+	open (OUT, ">$file_sjis") or die "$!";
+	foreach my $file ( sort keys %{$fileList} ){
+		my $sts = $fileList->{$file};
+		print OUT encode('cp932', "$file,$keys{$sts}\n");
+	}
+	close OUT;
+}
+
+sub _mkdir
+{
+	my $path = shift;
+	my @array = split /[\/\\]/, $path;
+	my $paths = '';
+	foreach my $dir (@array){
+		if( $dir ){
+			$paths = $paths."$dir";
+			mkdir encode('cp932', "$paths");
+#			print encode('cp932', "--- paths---\n[$paths]\n");
+		}
+		$paths = $paths."\/";
+	}
+}
+
+sub exportFiles
+{
+	my ($rev, $address, $files, $path) = @_;
+
+	$path = $path."\/$rev";
+	foreach my $file (@{$files}){
+		my $dir = "$path\/$file";
+		$dir =~ s/^(.+)[\\\/].+$/$1/;
+		#mkpath encode('cp932', $dir);
+		_mkdir $dir;
+		
+		my $res = svnCmd("export", "-r $rev", "$address\/$file", "$path\/$file");
+		#print @{$res};
+	}
+}
+
+sub execCmd
+{
+	my ($cmd) = @_;
+	$cmd = encode('cp932', $cmd);
+	print "cmd : $cmd\n";
+	open my $rs, "$cmd 2>&1 |";
+	my @rlist = <$rs>;
+	my @out = ();
+	foreach my $line (@rlist){
+		push @out, decode('cp932', $line);
+	}
+	close $rs;
+	return \@out;
+#	return \@rlist;
+}
+
+sub execCmd2
+{
+	my ($cmd) = @_;
+	print encode('cp932', "cmd : $cmd\n");
+	my $res = `$cmd 2>&1`;
+	my @array = split /\n/,$res;
+	print @array;
+	return \@array;
+}
+
+# svn cmd [option] addres [args...]
+sub svnCmd
+{
+	my ($cmd, $option, $address, $arg) = @_;
+	my $user = getUserInfo();
+	my $svnCmd = "svn $cmd $user $option \"$address\" $arg";
+	return execCmd($svnCmd);
+}
+
+sub getUserInfo
+{
+	my $res = '';
+	if( exists $gOptions->{'-u'} ){
+		$res = "--username $gOptions->{'-u'}";
+	}
+	if( exists $gOptions->{'-p'} ){
+		$res = $res." --password  $gOptions->{'-p'}";
+	}
+	return $res;
+}
+
+sub isInstallSVN
+{
+	my $res = execCmd('svn');
+	if( $res->[0] =~ /svn help/ ){
+		return 1;
+	}
+	return 0;
+}
+
+sub getDiffFileList
+{
+	my ($array) = @_;
+	my @files = ();
+	foreach my $line (@{$array}){
+		if( $line =~ /^Index: (.+?)$/ ){
+			push @files, $1;
+		}
+	}
+	return \@files;
+}
+
+# ブランチの最初と最後のリビジョンを取得する
+sub getRevisionNumber
+{
+	my ($address) = @_;
+	# --stop-on-copy を指定するとブランチができたポイントまでとなる
+	# --verbose を指定すると追加／削除／変更がファイル単位で分かる
+	my $resArray = svnCmd("log", "--stop-on-copy --verbose", "$address", "");
+	
+	my @revs = ();
+	my %fileList = ();
+	while( my $line = pop(@{$resArray}) ){ # 過去からさかのぼる
+		# exe : r1993 | k-oya | 2013-07-22 22:24:36 +0900 (月, 22 7 2013) | 3 lines
+		if( $line =~ /^r([0-9]+) |.+ lines$/ ){
+			push @revs, $1;
+		}
+		elsif( $line =~ /^   ([MADR]{1}) (\/.+?)$/ ){
+			my $a1 = $1; my $a2 = $2;
+			if( $a2 =~ /\(from .+\)/ ){
+				next; # フォルダなんでfilelistの対象としない
+			}
+			# A 項目が追加されました。
+			# D 項目が削除されました。
+			# M 項目の属性やテキスト内容が変更されました。
+			# R 項目が同じ場所の違うもので置き換えられました。
+			if( exists $fileList{$a2} ){
+				if( $a1 =~ /^[AD]$/ ){
+					$fileList{$a2} = $a1;
+				}
+			}
+			else{
+				$fileList{$a2} = $a1;
+			}
+			
+		}
+	}
+	return ($revs[$#revs], $revs[0], \%fileList);
+}
+
+
+sub getOptions
+{
+	my ($argv) = @_;
+	my %options = ();
+	my @newAragv = ();
+	for(my $i=0; $i< @{$argv}; $i++){
+		my $key = decode('cp932', $argv->[$i]); # 入力アーギュメントは文字コードを変更してやらないとダメっぽい。
+		if( $key eq '-u' ){
+			$options{$key} = decode('cp932', $argv->[$i+1]);
+			$i++;
+		}
+		elsif( $key eq '-p' ){
+			$options{$key} = decode('cp932', $argv->[$i+1]);
+			$i++;
+		}
+		elsif( $key eq '-r' ){
+			my $param = decode('cp932', $argv->[$i+1]);
+			if( $param !~ /^(%d):(%d)$/ ) {
+				die "illigal parameter with options ($param)";
+			}
+			$options{'-r_start'} = $1;
+			$options{'-r_end'} = $2;
+			$i++;
+		}
+		elsif( $key eq '-report' ){
+			$options{'-report'} = decode('cp932', $argv->[$i+1]);
+			$i++;
+		}
+		
+		else{
+			push @newAragv, $key;
+		}
+	}
+	return (\@newAragv, \%options);
+}
+
